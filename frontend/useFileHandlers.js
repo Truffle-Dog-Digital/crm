6d87da2b9ee1cd963840ee28d1cb9980d136967d
Andrew Walker <andrew@almostawake.com>
2024-07-04 16:26:29 +1000

    useDragAndDrop factored out.

--- frontend/useFileHandlers.js
+++ frontend/useFileHandlers.js
@@ -1,9 +1,9 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { checkImport } from "./checkImport";
+import useDragAndDrop from "./useDragAndDrop";
 
 const useFileHandlers = (user, setLoading, setSummary) => {
   const [fileObjects, setFileObjects] = useState([]);
-  const [dragging, setDragging] = useState(false);
 
   const handleFileChange = async (event, handleCloseMenu) => {
     const file = event.target.files
@@ -32,57 +32,8 @@ const useFileHandlers = (user, setLoading, setSummary) => {
     if (handleCloseMenu) handleCloseMenu();
   };
 
-  const handleDragOver = (event) => {
-    event.preventDefault();
-    setDragging(true);
-    console.log("Dragging over");
-  };
-
-  const handleDragLeave = () => {
-    setDragging(false);
-    console.log("Drag leave");
-  };
-
-  const handleDrop = async (event) => {
-    event.preventDefault();
-    setDragging(false);
-    console.log("File dropped");
-    await handleFileChange(event);
-  };
-
-  const handlePaste = async (event) => {
-    event.preventDefault();
-    const text = event.clipboardData.getData("text");
-    try {
-      const rows = text.split("\n").filter((row) => row.trim() !== "");
-      const objects = rows.map((row) => JSON.parse(row));
-      setFileObjects(objects);
-
-      const checkResult = await checkImport(objects);
-      if (checkResult.error) {
-        setSummary({ error: checkResult.error });
-      } else {
-        setSummary({
-          totalObjects: checkResult.totalObjects,
-        });
-      }
-    } catch (error) {
-      console.error("Error processing clipboard content: ", error);
-      setSummary({ error: "Error processing clipboard content" });
-    }
-  };
-
-  useEffect(() => {
-    const handlePasteEvent = (e) => {
-      handlePaste(e);
-    };
-
-    window.addEventListener("paste", handlePasteEvent);
-
-    return () => {
-      window.removeEventListener("paste", handlePasteEvent);
-    };
-  }, [user]);
+  const { dragging, handleDragOver, handleDragLeave, handleDrop } =
+    useDragAndDrop(handleFileChange, setFileObjects, setSummary);
 
   return {
     fileObjects,
