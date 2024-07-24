e6bc4fed5adb4fc5eb4b83778d6fb62af7ab292e
Andrew Walker <andrew@almostawake.com>
2024-07-24 15:18:43 +1000

    Global idProduction working for scripts

--- extension/drawer.js
+++ extension/drawer.js
@@ -1,10 +1,26 @@
+// Global variable to store the state of the production switch
+var isProduction = false;
+
 function injectDrawer() {
   injectHTMLAndCSS("drawer.html", "drawer.css", "body")
     .then(() => {
       setupDrawerCloseButton();
+      setupProductionSwitch(); // Set up the event listener for the switch after the drawer is injected
       document.body.classList.add("reabilityDrawerOpen"); // Adjust body margin when drawer is injected
     })
     .catch((error) => console.error(error));
+}
+
+function setupProductionSwitch() {
+  const productionSwitch = document.getElementById("productionSwitch");
+
+  if (productionSwitch) {
+    // Add event listener for state changes
+    productionSwitch.addEventListener("change", function () {
+      isProduction = this.checked;
+      console.log("Production Switch is " + (isProduction ? "ON" : "OFF"));
+    });
+  }
 }
 
 function setupDrawerCloseButton() {
