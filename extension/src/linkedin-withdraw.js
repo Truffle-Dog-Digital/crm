a2c6632cf409dce615dd8ea5ef1dace6d8d3b11d
Andrew Walker <andrew@almostawake.com>
2024-08-05 20:33:14 +1000

    added a clipboard copy on sent invitations to grab profiles with pending invitations

--- extension/src/linkedin-withdraw.js
+++ extension/src/linkedin-withdraw.js
@@ -4,6 +4,11 @@ import {
   setupProductionSwitch,
   setupDrawerCloseButton,
 } from "./drawer.js";
+import {
+  copyToClipboard,
+  clearClipboard,
+  setupClipboardListener,
+} from "./helpers-clipboard.js";
 
 // Global variable to store the state of the production switch
 var isProduction = false;
@@ -44,6 +49,54 @@ async function handleWithdrawButtonClick() {
   }
 }
 
+// Function to handle clipboard copy event
+async function handleClipboardCopy(event) {
+  console.log("REABILITY: Clipboard copy detected");
+  const linkedinUrls = await getLinkedinInvitationCardPictures();
+  const lineDelimitedUrls = linkedinUrls.join("\n");
+  if (linkedinUrls.length > 0) {
+    await copyToClipboard(lineDelimitedUrls);
+    console.log(
+      "REABILITY: Line delimited URLs copied to clipboard",
+      lineDelimitedUrls
+    );
+  } else {
+    await clearClipboard();
+    console.log(
+      "REABILITY: No invitation card pictures found, clipboard cleared"
+    );
+  }
+}
+
+// Function to get LinkedIn invitation card pictures
+async function getLinkedinInvitationCardPictures() {
+  console.log("REABILITY: Getting LinkedIn invitation card pictures");
+
+  try {
+    const nodes = await waitForXPath(
+      "//a[contains(@class, 'invitation-card__picture')]/@href",
+      3000
+    );
+
+    const uniqueHrefs = new Set();
+
+    for (const node of nodes) {
+      if (node.value) {
+        uniqueHrefs.add(node.value);
+      }
+    }
+
+    const urlsArray = Array.from(uniqueHrefs).map(
+      (href) => `https://linkedin.com${new URL(href).pathname}`
+    );
+
+    return urlsArray;
+  } catch (error) {
+    console.error("Error finding invitation card pictures:", error);
+    return [];
+  }
+}
+
 // Inject the drawer HTML and CSS, and setup drawer functionality
 injectDrawer();
 setupProductionSwitch();
@@ -53,6 +106,7 @@ setupDrawerCloseButton();
 injectHTMLAndCSS("linkedin-withdraw.html", null, "#reabilityDrawerContent")
   .then(() => {
     setupWithdrawButton();
+    setupClipboardListener(handleClipboardCopy);
   })
   .catch((error) => {
     console.error("REABILITY: Error setting up Linkedin HTML and CSS:", error);
